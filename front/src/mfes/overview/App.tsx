f6cd83b9cd8d80881edd3e7184d3dd93383c82c4
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2025-09-24 00:42:13 +0500

    rollback unneeded changes

--- front/src/mfes/overview/App.tsx
+++ front/src/mfes/overview/App.tsx
@@ -1,42 +1,18 @@
 'use client';
 
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import OverviewPage from './components/overview';
-import { MicroFrontendErrorBoundary } from '../lib/micro-frontend-error-boundary';
-import {
-  MicroFrontendNotFound,
-  RouterLifecycleManager
-} from '../lib/router-lifecycle';
 
 import type { MicroFrontendRuntimeProps } from '../lib/types';
-
-const MFE_KEY = 'overview';
-
-function OverviewRoutes() {
-  const location = useLocation();
-  const resetKey = `${location.pathname}${location.search}`;
-
-  return (
-    <MicroFrontendErrorBoundary key={resetKey} mfeKey={MFE_KEY}>
-      <>
-        <RouterLifecycleManager mfeKey={MFE_KEY} />
-        <Routes>
-          <Route path='/' element={<OverviewPage />} />
-          <Route
-            path='*'
-            element={<MicroFrontendNotFound mfeKey={MFE_KEY} />}
-          />
-        </Routes>
-      </>
-    </MicroFrontendErrorBoundary>
-  );
-}
 
 export function App({ basename }: MicroFrontendRuntimeProps) {
   return (
     <BrowserRouter basename={basename ?? '/'}>
-      <OverviewRoutes />
+      <Routes>
+        <Route path='/' element={<OverviewPage />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- front/src/mfes/profile/App.tsx
+++ front/src/mfes/profile/App.tsx
@@ -1,42 +1,17 @@
 'use client';
 
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { ProfileScreen } from './screens/ProfileScreen';
-import { MicroFrontendErrorBoundary } from '../lib/micro-frontend-error-boundary';
-import {
-  MicroFrontendNotFound,
-  RouterLifecycleManager
-} from '../lib/router-lifecycle';
+import ProfileViewPage from './components/profile-view-page';
 
 import type { MicroFrontendRuntimeProps } from '../lib/types';
-
-const MFE_KEY = 'profile';
-
-function ProfileRoutes() {
-  const location = useLocation();
-  const resetKey = `${location.pathname}${location.search}`;
-
-  return (
-    <MicroFrontendErrorBoundary key={resetKey} mfeKey={MFE_KEY}>
-      <>
-        <RouterLifecycleManager mfeKey={MFE_KEY} />
-        <Routes>
-          <Route path='/' element={<ProfileScreen />} />
-          <Route
-            path='*'
-            element={<MicroFrontendNotFound mfeKey={MFE_KEY} />}
-          />
-        </Routes>
-      </>
-    </MicroFrontendErrorBoundary>
-  );
-}
 
 export function App({ basename }: MicroFrontendRuntimeProps) {
   return (
     <BrowserRouter basename={basename ?? '/'}>
-      <ProfileRoutes />
+      <Routes>
+        <Route path='/' element={<ProfileViewPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
